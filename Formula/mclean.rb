45adc3ad7ad5a3cf231beaff31fe76eba706f059
ANGRYCONE <theepicmanofficial@gmail.com>
2026-04-19 15:34:38 +1000

    Update Mclean formula for version 2.4.1

--- Formula/mclean.rb
+++ Formula/mclean.rb
@@ -1,18 +1,20 @@
 class Mclean < Formula
-  desc "Simple macOS system data cleaner"
-  homepage "https://github.com/ANGRYCONE/Simple-macOS-System-Data-Cleaner"
-  url "https://github.com/ANGRYCONE/Simple-macOS-System-Data-Cleaner/releases/download/v2.4/macos_cleaner.sh"
-  sha256 "97206ac4223af55a565c0291682c8d95ae74db5dea4c978ce45cfdc160a0e7c5"
-  version "2.4"
+  desc "macOS System Cleaner"
+  homepage "https://github.com/ANGRYCONE/macOS-System-Cleaner"
+  url "https://github.com/ANGRYCONE/macOS-System-Cleaner/releases/download/v2.4.1/macos_cleaner.sh"
+  sha256 "f0233127b01bed211ca1e8ac8b33fe2cc3b5a78646dab642be5abee89cb2298f"
+  version "2.4.1"
 
   depends_on :macos
 
   def install
     libexec.install "macos_cleaner.sh"
+
     (bin/"mclean").write <<~SH
       #!/bin/sh
       exec "#{libexec}/macos_cleaner.sh" --skip-update "$@"
     SH
+
     chmod 0755, libexec/"macos_cleaner.sh"
   end
 end
